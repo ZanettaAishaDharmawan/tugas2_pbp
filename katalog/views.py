70f90ae66b5a181f326bcf1643bdf615333a2848
zanetta.dharmawan636 <zanetta.dharmawan636@gmail.com>
2022-09-11 20:12:05 +0700

    commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+import imp
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Zanetta Aisha Dharmawan',
+        'npm': '2106751846'
+    }
+    return render(request, "katalog.html",context)
